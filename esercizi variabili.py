0365f4b728ca2e71673e339f789205e61430a185
nausicaadeca-commits <nausicaadeca@gmail.com>
2026-04-12 15:38:51 +0200

    Operazioni con variabili
    Esecuzione di semplici operazioni con variabili numeriche, come somme, moltiplicazioni e confronti.

---
+++ esercizi variabili.py
@@ -0,0 +1,30 @@
+# esercizio 1
+
+x = 7
+y = 3
+
+somma = (x + y)
+print(somma)
+
+# esercizio 2
+
+a = 19.99
+b = 3
+
+somma = a * b
+print(somma)
+
+#esercizio 3
+
+a= 5 
+b = 2.5
+
+somma = a + b
+print(somma)
+
+#esercizio 4
+
+a = 10
+b = 5
+
+print(a > b)
