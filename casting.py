bb39abcb906bb8d03cbd7f0be7b448313a00eddc
nausicaadeca-commits <nausicaadeca@gmail.com>
2026-04-12 15:20:15 +0200

    inserisci un numero e lo stampa a video

---
+++ casting.py
@@ -0,0 +1,15 @@
+#esercizio 1
+
+print("ciao, inserisci un numero intero")
+
+testo_utente = input("Tu:")
+
+n_testo = str(testo_utente)
+
+n_int = int(n_testo)
+
+num_float = float(n_int)
+
+print(f"il numero che hai inserito è {num_float}")
+
+print(num_float)
