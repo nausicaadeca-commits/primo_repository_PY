0b1c3341e19b0aa17146ccc4a34e14e1e3ac9e0f
nausicaadeca-commits <nausicaadeca@gmail.com>
2026-04-12 15:50:34 +0200

    Variabili e tipi di dati
    Serie di esercizi sulle variabili e sui tipi di dati: creazione e aggiornamento di variabili, operazioni aritmetiche, scambio di valori, concatenazione e ripetizione di stringhe, confronti booleani e conversione tra tipi di dato (casting).

---
+++ Variabili_Tipi_di_dati_casting.py
@@ -0,0 +1,110 @@
+#Primo Esercizio
+#Creare tre variabili: nome (stringa), età (intero) e città (stringa).Stampare le variabili.
+
+nome = ("nausicaa")
+età = ("19")
+città = ("lecco")
+
+print(nome)
+print(età)
+print(città)
+
+#Secondo Esercizio
+#Creare una variabile x, assegnarle un valore e aggiornarla. Stampare il valore prima e dopo l'aggiornamento.
+
+x = 5
+print(5)
+x = 15
+print(x)
+
+#Terzo Esercizio
+#Somma di due variabili intere e stampa del risultato. 
+
+x = 5
+y = 10
+
+somma = x + y
+print(somma)
+
+# Quarto Esercizio
+#Scambio dei valori tra due variabili x e y.
+
+x = 3
+y = 4
+
+x, y = y, x
+print(x, y)
+
+#Quinto Esercizio
+#Calcolo dell'area di un rettangolo utilizzando base e altezza.
+
+base = 10
+altezza = 20
+
+area = base + altezza
+print(area)
+
+#Sesto Esercizio
+#Somma di un numero intero con un decimale e stampa del risultato.
+
+x = 5
+y = 2.5
+
+somma = (x + y)
+print(somma)
+
+#Settimo Esercizio
+#Calcolare la media di tre numeri.
+
+x = 3
+y = 7
+z = 50
+
+media = (x + y + z)/ 3
+print(media)
+
+#Ottavo Esercizio
+#Concatenazione di due stringhe.
+
+x = ("ciao ")
+y = ("mattia")
+
+print(x + y)
+
+#Nono Esercizio
+#Ripetizione della parola "ciao" tre volte utilizzando una stringa.
+
+print("ciao " * 3)
+
+#Decimo Esercizio
+#Uso di variabili boolean per verificare se un numero è maggiore di un altro.
+
+n1 = 20
+n2 = 50
+n3 = 70
+
+print(n1 < n2)
+print(n2 > n3)
+print(n1 < n3)
+
+#Undicesimo Esercizio
+#Trasformare un intero in un float e stamparlo.
+
+x = 10
+f_numero = float(x)
+print(f_numero)
+
+#Dodicesimo Esercizio
+#Convertire un numero in una stringa e stamparlo.
+
+x = 15
+s_stringa = str(x)
+print(s_stringa)
+
+#Tredicesimo Esercizio
+#Uso di variabili booleane per valutare valori positivi, negativi, nulli e una stringa vuota
+
+print (bool(1))
+print (bool(0))
+print (bool(-20))
+print (bool(""))
